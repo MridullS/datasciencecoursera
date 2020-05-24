81b6fff482a244a4d9077b68214bd4983853338f
Mridull Saraff <46999589+MridullS@users.noreply.github.com>
2020-05-24 21:43:20 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Assignment: Caching the Inverse of a Matrix
+
+# Matrix inversion is usually a costly computation and there may be some
+# benefit to caching the inverse of a matrix rather than computing it
+# repeatedly (there are also alternatives to matrix inversion that we will
+# not discuss here). Your assignment is to write a pair of functions that
+# cache the inverse of a matrix.
 
 ## Write a short comment describing this function
 
